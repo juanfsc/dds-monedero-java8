d751aca6ec76fc7d063dff121811b471c71e9ab7
Juan Salazar <juanfrancisco14sc@gmail.com>
2022-05-13 23:07:55 -0300

    feature envy en Cuenta.getMontoExtraidoA(LocalDate)

--- src/main/java/dds/monedero/model/Cuenta.java
+++ src/main/java/dds/monedero/model/Cuenta.java
@@ -47,7 +47,7 @@ public class Cuenta {
 
   public double getMontoExtraidoA(LocalDate fecha) {
     return getMovimientos().stream()
-        .filter(movimiento -> !movimiento.isDeposito() && movimiento.getFecha().equals(fecha))
+        .filter(movimiento -> !movimiento.extraidoEn(fecha))
         .mapToDouble(Movimiento::getMonto)
         .sum();
   }
